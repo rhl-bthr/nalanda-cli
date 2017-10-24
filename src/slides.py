7c734dfe92af5e25df98d6a70c23e137636727a5
Pro-Panda <f2016015@pilani.bits-pilani.ac.in>
2017-10-24 20:41:56 +0530

    Fixed slides download message display

--- src/slides.py
+++ src/slides.py
@@ -3,6 +3,7 @@ import os
 
 
 def download(session, sub_names, resource_urls, path):
+    subject_updates = [len(x) for x in resource_urls]
     for x in range(len(sub_names)):
         for y in range(len(resource_urls[x])):
             if ('folder/view' in resource_urls[x][y]):
@@ -15,8 +16,10 @@ def download(session, sub_names, resource_urls, path):
                 1].split('"')[0]
             with open(os.path.join(path, sub_names[x], file_name), 'wb') as f:
                 f.write(result.content)
+    return subject_updates
+
 
 
 def main(session, sub_names, resource_urls, path):
-    terminal_display(None, 'Lecture-Slides', sub_names, " has new Updates")
-    download(session, sub_names, resource_urls, path)
+    subject_updates = download(session, sub_names, resource_urls, path)
+    terminal_display(None, 'Lectures', sub_names, " has new updates", subject_updates)
