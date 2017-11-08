4c6d87d74a4a794ad6e43e8d834bc24e767e5ea0
Pro-Panda <f2016015@pilani.bits-pilani.ac.in>
2017-11-08 13:53:58 +0530

    Update: Unified Exception Handling on main, Update: Added List comprehension

--- src/slides.py
+++ src/slides.py
@@ -1,32 +1,31 @@
 import io, os, requests
 
-INSTALLATION_FOLDER = os.path.join(os.path.expanduser('~'), '.termi-nalanda')
+join = os.path.join
+INSTALL_PATH = join(os.path.expanduser('~'), '.termi-nalanda')
+
 def download(session, sub_names, res_urls, path):
-    subject_updates = [[] for x in sub_names]
+    sub_updates = [[] for x in sub_names]
     for x in range(len(sub_names)):
-        done_slides_file = io.open(os.path.join(INSTALLATION_FOLDER,"Lectures",sub_names[x]+'.txt'),'a+')
+        done_slides_file = io.open(join(INSTALL_PATH,"Lectures",sub_names[x]+'.txt'),'a+')
         done_slides_file.seek(0)
         done_slides = done_slides_file.read().split('\n')
         for y in range(len(res_urls[x])):
             if (res_urls[x][y] not in done_slides):
                 if ('folder/view' in res_urls[x][y]):
                     id_param = res_urls[x][y].split('php')[1]
-                    try:
-                        result = session.get(
-                        'http://nalanda.bits-pilani.ac.in/mod/folder/download_folder.php' + id_param)
-                    except requests.exceptions.ConnectionError:
-                        quit("No Internet Connection. Please retry")
+                    result = session.get(
+                    'http://nalanda.bits-pilani.ac.in/mod/folder/download_folder.php' + id_param)
                 else:
                     result = session.get(res_urls[x][y])
                 file_name = result.headers['content-disposition'].split('e="')[
                     1].split('"')[0]
-                with io.open(os.path.join(path, sub_names[x], file_name), 'wb') as f:
+                with io.open(join(path, sub_names[x], file_name), 'wb') as f:
                     f.write(result.content)
                 done_slides_file.write(res_urls[x][y]+'\n')
-                subject_updates[x].append([])
-    return subject_updates
+                sub_updates[x].append([])
+    return sub_updates
 
 def main(session, sub_names, res_urls, path):
-    from terminal import terminal_display
+    from updates import term_display
     update_list = download(session, sub_names, res_urls, path)
-    terminal_display(update_list, 'Lectures', sub_names, " has new updates")
+    term_display(update_list, 'Lectures', sub_names, " has new updates")
--- src/terminal.py
+++ src/terminal.py
@@ -3,108 +3,76 @@
 # Termi-Nalanda
 
 from __future__ import print_function
-try:
-    from bs4 import BeautifulSoup
-    import os, io, requests
+from bs4 import BeautifulSoup
+import os, io, requests
+import updates
 
-    import slides, notices
+join = os.path.join
 
-    INSTALLATION_FOLDER = os.path.join(os.path.expanduser('~'), '.termi-nalanda')
-    def bold(text):
-        return '\033[1m' + text + '\033[0m'
+INSTALL_PATH = join(os.path.expanduser('~'), '.termi-nalanda')
 
-    def login():
-        session = requests.session()
-        try:
-            config = io.open(os.path.join(INSTALLATION_FOLDER, 'config.txt'), 'r')
-            config = (config.read()).split('\n')
-            session.post('http://nalanda.bits-pilani.ac.in/login/index.php', data={
-                'username': config[0],
-                'password': config[1],
-            })
-        except requests.exceptions.ConnectionError:
-            quit("No Internet Connection. Please retry")
-        except IOError:
-            quit("Unable to read from file. Please reinstall termi-Nalanda")
-        return config[2], session
+def login():
+    session = requests.session()
+    config = io.open(join(INSTALL_PATH, 'config.txt'), 'r')
+    config = (config.read()).split('\n')
+    session.post('http://nalanda.bits-pilani.ac.in/login/index.php', data={
+        'username': config[0],
+        'password': config[1],
+    })
+    return config[2], session
 
-    # Updating Subject List and making folders
-    def subject_list_folders(slides_path):
-        try:
-            name_file = io.open(
-            os.path.join(
-                INSTALLATION_FOLDER,
-                'Subjects','name.txt'),
-            'r')
-            url_file = io.open(os.path.join(INSTALLATION_FOLDER, 'Subjects','url.txt'), 'r')
-        except IOError:
-            quit("Unable to read from file. Please reinstall termi-Nalanda")
-        url_list = (url_file.read()).split('\n')
-        subject_list = (name_file.read()).split('\n')
-        for subject in subject_list:
-            subject_path = os.path.join(slides_path, subject)
-            if not os.path.exists(subject_path):
-                os.makedirs(subject_path)
-        return subject_list, url_list
+def sub_list_folders(slides_path):
+    """Updating Subject List and making folders"""
+    name_file = io.open(join(INSTALL_PATH,'Subjects','name.txt'),'r')
+    url_file = io.open(join(INSTALL_PATH, 'Subjects','url.txt'), 'r')
+    url_list = (url_file.read()).split('\n')
+    sub_list = (name_file.read()).split('\n')
+    for sub in sub_list:
+        sub_path = join(slides_path, sub)
+        if not os.path.exists(sub_path):
+            os.makedirs(sub_path)
+    return sub_list, url_list
 
-    # Getting all relevant links in each subject page
-    def get_all_links(subject_urls, session):
-        subject_links = []
-        for subj in subject_urls:
-            result = session.get(subj)
-            soup = BeautifulSoup(result.text, "html.parser")
-            subject_links.append(soup.find_all('a', {'onclick': ""}))
-        return subject_links
+def get_all_links(sub_urls, session):
+    """Getting all relevant links in each subject page"""
+    sub_links = [
+        BeautifulSoup(session.get(sub).text, "html.parser")
+        .find_all('a', {'onclick': ""})
+        for sub in sub_urls
+    ]
+    return sub_links
 
-    # Sorting Urls
-    def sorting_links(subject_links):
-        res_urls, news_urls, notice_urls = ([[] for x in range(len(subject_links))] for y in range(3))
-        for x in range(len(subject_links)):
-            for y in range(len(subject_links[x])):
-                url = (subject_links[x][y]).get('href')
-                if('resource/view.php?id' in url or 'folder/view.php?id=' in url):
-                    res_urls[x].append(url)
-                elif('page/view.php?id' in url):
-                    notice_urls[x].append(
-                        [url, subject_links[x][y].contents[1].contents[0]])
-                elif('forum/view.php?id' in url):
-                    news_urls[x].append(url)
-                #Needs to be worked upon and added at a later stage.
-                # elif('/mod/' in url and 'id' in url and 'index' not in url):
-                #     notice_urls[x].append([url, subject_links[x][y].contents])
-        return notice_urls, news_urls, res_urls
+def sorting_links(sub_links):
+    res_urls, news_urls, notice_urls = ([[] for x in range(len(sub_links))] for y in range(3))
+    for x in range(len(sub_links)):
+        for y in range(len(sub_links[x])):
+            url = (sub_links[x][y]).get('href')
+            if('resource/view.php?id' in url or 'folder/view.php?id=' in url):
+                res_urls[x].append(url)
+            elif('page/view.php?id' in url):
+                notice_urls[x].append(
+                    [url, sub_links[x][y].contents[1].contents[0]])
+            elif('forum/view.php?id' in url):
+                news_urls[x].append(url)
+            #Needs to be worked upon and added at a later stage.
+            # elif('/mod/' in url and 'id' in url and 'index' not in url):
+            #     notice_urls[x].append([url, sub_links[x][y].contents])
+    return (notice_urls, news_urls, res_urls)
 
-    def terminal_display(update_list=None,update_type=None,
-            sub_names=[], message=None):
-        print (bold(update_type+':'))
-        check_for_no_update = sum([len(x) for x in update_list])
-        if(check_for_no_update == 0):
-            print ("\tNo new " + update_type)
-            return 0
-        if (message is None):
-            for x in range(len(sub_names)):
-                for y in range(len(update_list[x])):
-                    if(y == 0):
-                        print (bold('\n'+sub_names[x] + '-'))
-                    print ('\t' + bold(str(y + 1))+'. ' + update_list[x][y][1])
-                    print ("\t\t" + update_list[x][y][0])
-        else:
-            for x in range(len(sub_names)):
-                if(len(update_list[x]) != 0):
-                    print (bold(sub_names[x]) + message)
-        print ('-' * 60 + '\n')
-        
-    def main():
-        """Displaying notices, news and other announcements, updating slides"""
-        print ("\t\t" + bold("**Nalanda**"))
+def main():
+    """Displaying notices, news and other announcements, updating slides""" 
+    try:
         slides_path, session = login()
-        subject_names, subject_urls = subject_list_folders(slides_path)
-        subject_links = get_all_links(subject_urls, session)
-        notice_urls, news_urls, res_urls = sorting_links(subject_links)
-        notices.main(session, subject_names, notice_urls, news_urls)
-        slides.main(session, subject_names, res_urls, slides_path)
+        sub_names, sub_urls = sub_list_folders(slides_path)
+        sub_links = get_all_links(sub_urls, session)
+        sorted_links = sorting_links(sub_links)
+        updates.main(session, sub_names, sorted_links, slides_path)
+    except requests.exceptions.ConnectionError:
+        quit("No Internet Connection. Please retry")
+    except IOError:
+        quit("Unable to read from file. Please reinstall termi-Nalanda")
+    except KeyboardInterrupt:
+        print("Stopped by user.")
 
-    if(__name__ == '__main__'):
-        main()
-except KeyboardInterrupt:
-    print("Stopped by user.")
+if(__name__ == '__main__'):
+    main()
