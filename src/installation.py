d13380da859ad59aabf75b3bccdc822041fe6487
Pro-Panda <f2016015@pilani.bits-pilani.ac.in>
2017-11-08 13:54:32 +0530

    Fix: Report for invalid password

--- src/installation.py
+++ src/installation.py
@@ -1,64 +1,82 @@
 try:
     from bs4 import BeautifulSoup
     import os, io, requests
-    from terminal import login
+except ImportError:
+    quit("Required Libraries aren't installed. Please restart the installaition")
 
-    INSTALLATION_FOLDER = os.path.join(os.path.expanduser('~'), '.termi-nalanda')
-    FOLDER_LIST = ['News', 'Notices', 'Lectures', 'Subjects']
+join = os.path.join
+INSTALL_PATH = join(os.path.expanduser('~'), '.termi-nalanda')
+FOLDER_LIST = ['News', 'Notices', 'Lectures', 'Subjects']
+
+try:
+    input = raw_input
+except NameError:
+    pass
+
+def take_config():
+    email = input("\nEnter BITS ID [Eg: f2016015]\n") + '@pilani.bits-pilani.ac.in'
+    pwd = input("Enter Nalanda Password\n")
+    path = input("Enter path to store lecture slides [Refer Readme]\n")
+    config_path = join(INSTALL_PATH, 'config.txt')
+    path = join(os.path.expanduser('~'), path)
+    f = io.open(config_path, 'w')
+    config = (email + '\n' + pwd + '\n' + path)
     try:
-        input = raw_input
-    except NameError:
-        pass
+        f.write(config)
+    except TypeError:
+        f.write(unicode(config))
+    f.close()
 
-    def take_config():
-        email = input("\nEnter your BITS ID [Eg: f2016015]\n") + '@pilani.bits-pilani.ac.in'
-        pwd = input("Enter your Nalanda Password\n")
-        path = input("Enter the path to store the lecture slides [Refer to readme]\n")
-        config_path = os.path.join(INSTALLATION_FOLDER, 'config.txt')
-        path = os.path.join(os.path.expanduser('~'), path)
-        f = io.open(config_path, 'w')
-        config = (email + '\n' + pwd + '\n' + path)
-        try:
-            f.write(config)
-        except TypeError:
-            f.write(unicode(config))
+def make_folders():
+    for folder_name in FOLDER_LIST:
+        os.makedirs(join(INSTALL_PATH, folder_name))
 
-        f.close()
+def get_sub_list(session):
+    url_file_name = join(INSTALL_PATH, 'Subjects', 'url.txt')
+    name_file_name = join(INSTALL_PATH, 'Subjects', 'name.txt')
+    url_file = io.open(url_file_name, 'w')
+    name_file = io.open(name_file_name, 'w')
+    result = session.get('http://nalanda.bits-pilani.ac.in/my')
+    soup = BeautifulSoup(result.text, "html.parser")
+    sub_url, sub_name = [],[]
+    for x in soup.find_all('div', 'column c1'):
+        sub_url.append(x.contents[0].get('href'))
+        sub_name.append(((x.contents[0].contents[1]).split('/')[0]).split('\\')[0])
+    try:
+        url_file.write('\n'.join(sub_url))
+        name_file.write('\n'.join(sub_name))
+    except TypeError:
+        url_file.write(unicode('\n'.join(sub_url)))
+        name_file.write(unicode('\n'.join(sub_name)))
 
-    def make_folders():
-        for folder_name in FOLDER_LIST:
-            os.makedirs(os.path.join(INSTALLATION_FOLDER, folder_name))
+def login():
+    session = requests.session()
+    config = io.open(join(INSTALL_PATH, 'config.txt'), 'r')
+    config = (config.read()).split('\n')
+    result = session.post('http://nalanda.bits-pilani.ac.in/login/index.php', data={
+        'username': config[0],
+        'password': config[1],
+    })
+    result = BeautifulSoup(result.text,"html.parser")
+    login_check = result.find_all('a',{'id':"loginerrormessage"})
+    if(login_check):
+        print("Username or Password Incorrect. Please retry")
+        take_config()
+        login()
+    return config[2], session
 
-    def get_subject_list(session):
-        url_file_name = os.path.join(INSTALLATION_FOLDER, 'Subjects', 'url.txt')
-        name_file_name = os.path.join(INSTALLATION_FOLDER, 'Subjects', 'name.txt')
-        try:
-            url_file = io.open(url_file_name, 'w')
-            name_file = io.open(name_file_name, 'w')
-            result = session.get('http://nalanda.bits-pilani.ac.in/my')
-        except IOError:
-            quit("Unable to read from file. Please reinstall termi-Nalanda")
-        except requests.exceptions.ConnectionError:
-            quit("No Internet Connection. Please retry")
-
-        soup = BeautifulSoup(result.text, "html.parser")
-        subject_url, subject_name = [],[]
-        for x in soup.find_all('div', 'column c1'):
-            subject_url.append(x.contents[0].get('href'))
-            subject_name.append(((x.contents[0].contents[1]).split('/')[0]).split('\\')[0])
-        url_file.write('\n'.join(subject_url))
-        name_file.write('\n'.join(subject_name))
-
-    def main():
+def main():
+    try:
+        make_folders()
         take_config()
         dump, session = login()
-        make_folders()
-        get_subject_list(session)
+        get_sub_list(session)
+    except KeyboardInterrupt:
+        print "Installation cancelled by user. Plase retry"
+    except requests.exceptions.ConnectionError:
+        quit("No Internet Connection. Please retry")
+    except IOError:
+        quit("Unable to read from file. Please reinstall termi-Nalanda")
 
-    if(__name__ == '__main__'):
-        main()
-
-except ImportError as e:
-    print e
-except KeyboardInterrupt:
-    print "Installation cancelled by user. Plase retry"
+if(__name__ == '__main__'):
+    main()
