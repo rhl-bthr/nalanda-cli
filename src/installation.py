4ac56932786c9338c3345035981b3cde28f4a1a2
Rahul Bothra <rrbothra@gmail.com>
2018-09-13 19:20:06 +0530

    refactor installation.py

--- src/installation.py
+++ src/installation.py
@@ -1,92 +1,64 @@
+import os
+from getpass import getpass
+
 try:
     from bs4 import BeautifulSoup
-    import os
-    import io
     import requests
 except ImportError:
     quit("Required Libraries aren't installed. Please restart installation.")
 
 join = os.path.join
 INSTALL_PATH = join(os.path.expanduser("~"), ".nalanda-cli")
+session = requests.session()
+
 FOLDER_LIST = ["News", "Notices", "Lectures", "Subjects"]
 
-try:
-    input = raw_input
-except NameError:
-    pass
 
-
-def take_config():
-    email = input("\nEnter BITS ID [Eg: f2016015]\n") + \
-        "@pilani.bits-pilani.ac.in"
-    pwd = input("Enter Nalanda Password\n")
-    config_path = join(INSTALL_PATH, "config.txt")
-    f = io.open(config_path, "w")
-    config = (email + "\n" + pwd)
+if __name__ == "__main__":
     try:
-        f.write(config)
-    except TypeError:
-        f.write(unicode(config))
-    f.close()
-
-
-def make_folders():
-    for folder_name in FOLDER_LIST:
+        for folder_name in FOLDER_LIST:
         os.makedirs(join(INSTALL_PATH, folder_name))
 
+        while True:
+            email = input("\nEnter BITS ID [Eg: f2016015]\n")
+            email += "@pilani.bits-pilani.ac.in"
 
-def get_sub_list(session):
-    url_file_name = join(INSTALL_PATH, "sub-url.txt")
-    name_file_name = join(INSTALL_PATH, "sub-name.txt")
-    url_file = io.open(url_file_name, "w")
-    name_file = io.open(name_file_name, "w")
-    result = session.get("http://nalanda.bits-pilani.ac.in/my")
-    soup = BeautifulSoup(result.text, "html.parser")
-    sub_url, sub_name = [], []
-    for x in soup.find_all("div", "column c1"):
-        sub_url.append(x.contents[0].get("href"))
-        sub_name.append(
-            ((x.contents[0].contents[1]).split("/")[0]).split("\\")[0])
-    try:
-        url_file.write("\n".join(sub_url))
-        name_file.write("\n".join(sub_name))
-    except TypeError:
-        url_file.write(unicode("\n".join(sub_url)))
-        name_file.write(unicode("\n".join(sub_name)))
+            password = getpass(prompt = "Enter nalanda password:")
 
+            with open(join(INSTALL_PATH, "config.txt"), "w") as f:
+                config = email + "\n" + password
+                f.write(config)
 
-def login():
-    session = requests.session()
-    config = io.open(join(INSTALL_PATH, "config.txt"), "r")
-    config = (config.read()).split("\n")
-    result = session.post(
-        "http://nalanda.bits-pilani.ac.in/login/index.php",
-        data={
-            "username": config[0],
-            "password": config[1],
-        })
-    result = BeautifulSoup(result.text, "html.parser")
-    login_check = result.find_all("a", {"id": "loginerrormessage"})
-    if(login_check):
-        print("Username or Password Incorrect. Please retry")
-        take_config()
-        login()
-    return config[2], session
+            result = session.post(
+                "http://nalanda.bits-pilani.ac.in/login/index.php",
+                data={
+                    "username": email,
+                    "password": password,
+                })
+            result = BeautifulSoup(result.text, "html.parser")
 
+            if not result.find_all("a", {"id": "loginerrormessage"}):
+                break
+            print("Username or Password Incorrect. Please retry")
 
-def main():
-    try:
-        make_folders()
-        take_config()
-        dump, session = login()
-        get_sub_list(session)
+        with open(join(INSTALL_PATH, "sub-url.txt"), "w") as url_file:
+            with open(join(INSTALL_PATH, "sub-name.txt"), "w") as name_file:
+
+                result = session.get("http://nalanda.bits-pilani.ac.in/my")
+                soup = BeautifulSoup(result.text, "html.parser")
+
+                sub_url, sub_name = [], []
+
+                for x in soup.find_all("div", "column c1"):
+                    sub_url.append(x.contents[0].get("href"))
+                    sub_name.append(((x.contents[0].contents[1]).split("/")[0]).split("\\")[0])
+
+                url_file.write("\n".join(sub_url))
+                name_file.write("\n".join(sub_name))
+
     except KeyboardInterrupt:
-        print("Installation cancelled by user. Please retry.")
+        quit("Installation cancelled by user. Please retry.")
     except requests.exceptions.ConnectionError:
         quit("No Internet Connection. Please retry.")
     except IOError:
-        quit("Unable to read from file. Please reinstall termi-Nalanda.")
-
-
-if(__name__ == "__main__"):
-    main()
+        quit("Unable to read from file. Please reinstall nalanda-cli.")
