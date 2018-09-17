9cd884965dc6e6ab270768c6202b13c329bea19d
Rahul Bothra <rrbothra@gmail.com>
2018-09-17 11:01:25 +0530

    apply indenation to json stored data

--- src/installation.py
+++ src/installation.py
@@ -41,11 +41,10 @@ try:
         os.makedirs(INSTALL_PATH)
 
     with open(CONFIG_FILE, "w") as f:
-        json.dump(config, f)
+        json.dump(config, f, indent=4)
 
     result = session.get(HOMEPAGE_LINK)
     soup = BeautifulSoup(result.text, "html.parser")
-
     for x in soup.find_all("div", "column c1"):
         sub_name_url[x.contents[0].get("href")] = ((x.contents[0].contents[1]).split("/")[0]).split("\\")[0]
         URLS[x.contents[0].get("href")] = {
@@ -54,9 +53,9 @@ try:
             "news":[]
         }
 
-    json.dump(URLS, open(DATA_FILE, 'w'))
-    json.dump(sub_name_url, open(SUBJECTS_FILE, "w"))
-
+    json.dump(URLS, open(DATA_FILE, 'w'), indent=4)
+    json.dump(sub_name_url, open(SUBJECTS_FILE, "w"), indent=4)
+    print("\nInstallation Successful ✔")
 
 except KeyboardInterrupt:
     quit("Installation cancelled by user. Please retry.")
